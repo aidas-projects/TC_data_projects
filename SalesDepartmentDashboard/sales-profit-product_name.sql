b42e05be03f751389c78e4315f196f1cd8f7df20
Aidas Rasimas <126146031+aidas-projects@users.noreply.github.com>
2023-08-06 09:32:16 +0300

    Create sales-profit-product_name.sql

---
+++ SalesDepartmentDashboard/sales-profit-product_name.sql
@@ -0,0 +1,42 @@
+SELECT
+  OrderDate,
+  CASE 
+  WHEN  SalesPersonID IS NULL
+  THEN 'Online'
+  ELSE 'Offline'
+  END AS order_type,
+  reason.Name AS sales_reason,
+  sales.salesorderid,
+  salesorderdetail.orderqty,
+  sales.TotalDue,
+  sales.SubTotal,
+  salesorderdetail.linetotal,
+  salesorderdetail.unitprice,
+  product.standardcost,
+  (salesorderdetail.linetotal - product.standardcost*salesorderdetail.orderqty) AS profit,
+  REGEXP_REPLACE(product.name, ',.*', '') AS product_name
+  
+
+FROM
+tc-da-1.adwentureworks_db.salesorderheader AS sales
+LEFT JOIN
+tc-da-1.adwentureworks_db.salesorderheadersalesreason AS sales_reason
+ON
+sales.SalesOrderID = sales_reason.salesOrderID
+LEFT JOIN
+tc-da-1.adwentureworks_db.salesreason AS reason
+ON
+sales_reason.SalesReasonID = reason.SalesReasonID
+
+LEFT JOIN
+tc-da-1.adwentureworks_db.salesorderdetail
+ON sales.salesorderid = salesorderdetail.salesorderid
+
+LEFT JOIN tc-da-1.adwentureworks_db.product
+ON salesorderdetail.productid = product.productid
+
+LEFT JOIN tc-da-1.adwentureworks_db.specialoffer
+ON salesorderdetail.specialofferid = specialoffer.specialofferid
+
+ORDER BY OrderDate, salesorderid
+
